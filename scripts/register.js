5c7da70e633ae93d0c56b1779d9d9d9f04f8857d
fernandoderrigo <fer.rainbow.hair@gmail.com>
2024-06-29 15:13:07 -0300

    Parche para register

--- scripts/register.js
+++ scripts/register.js
@@ -1,26 +1,24 @@
 document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    const email = document.getElementById('email').value;
-    const username = document.getElementById('username').value;
-    const name = document.getElementById('name').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-  
-    // Validar que todos los campos estén completos
-    if (!email || !username || !name || !password || !confirmPassword) {
-      alert('Por favor, completa todos los campos.');
-      return;
-    }
-  
-    // Validar que las contraseñas coincidan
-    if (password !== confirmPassword) {
-      alert('Las contraseñas no coinciden.');
-      return;
-    }
-  
-    // Si todo es válido, puedes enviar el formulario
-    alert('Registro exitoso');
+  event.preventDefault();
 
-  });
-  
+  const email = document.getElementById('email').value;
+  const username = document.getElementById('username').value;
+  const name = document.getElementById('name').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
+
+  // Validar que todos los campos estén completos
+  if (!email || !username || !name || !password || !confirmPassword) {
+    alert('Por favor, completa todos los campos.');
+    return;
+  }
+
+  // Validar que las contraseñas coincidan
+  if (password !== confirmPassword) {
+    alert('Las contraseñas no coinciden.');
+    return;
+  }
+
+  // Si todo es válido, puedes enviar el formulario
+  alert('Registro exitoso');
+});
